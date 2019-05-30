7b0502662cd176b8095619b536c9ba854d7cbe9a
angmayor <51034211+angmayor@users.noreply.github.com>
2019-05-30 13:44:13 -0400

    Add files via upload

---
+++ Matriz2.js
@@ -0,0 +1,7 @@
+var concierto = ["Lunes,Martes,Miercoles,Jueves,Viernes,Sabado,Domingo"["Madrid.Sevilla.Barcelona.Valencia.Zaragoza.Bilbao.Donosti"]];
+
+concierto = split(",");
+alert(concierto[3]);
+
+
+
